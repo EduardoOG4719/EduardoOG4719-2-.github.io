c26ecd8d9ef97656409cb8b4be826f4e33c88007
EduardoOG4719 <laloniuxolmedo@gmail.com>
2022-12-08 20:13:40 -0600

    add idiomas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WorkExperiencieComponent } from './work-experiencie/work-experiencie.component';
 import { EducationComponent } from './education/education.component';
+import { IdiomasComponent } from './idiomas/idiomas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     WorkExperiencieComponent,
-    EducationComponent
+    EducationComponent,
+    IdiomasComponent
   ],
   imports: [
     BrowserModule
